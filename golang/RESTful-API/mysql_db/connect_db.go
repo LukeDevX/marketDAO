93343470740df23670fcd12c6b8227ce8b2ede5b
luke <luke-github@outlook.com>
2025-03-15 19:14:26 +0800

    update directory

---
+++ golang/RESTful-API/mysql_db/connect_db.go
@@ -0,0 +1,70 @@
+package mysql_db
+
+//package main
+
+import (
+	"fmt"                    // 标准库，提供格式化 I/O 功能，例如打印日志和调试输出
+	"github.com/spf13/viper" // Viper 库，用于读取和解析配置文件（YAML、JSON、TOML 等）
+	"gorm.io/driver/mysql"   // GORM 的 MySQL 连接驱动，用于连接 MySQL 数据库
+	"gorm.io/gorm"           // GORM（Go 的 ORM 框架），用于数据库操作
+	"gorm.io/gorm/schema"    // GORM 的 Schema 相关功能，比如自定义表名规则
+	"time"                   // 标准库，提供时间和日期处理
+)
+
+func ConnectToDatabase() (*gorm.DB, error) {
+	//链接数据库
+	// 参考 https://github.com/go-sql-driver/mysql#dsn-data-source-name 获取详情
+	//dsn := "root:123456@tcp(127.0.0.1:3306)/crud-list?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=True&loc=Local",
+		viper.GetString("db.UserName"),
+		viper.GetString("db.Password"),
+		viper.GetString("db.Host"),
+		viper.GetString("db.Port"),
+		viper.GetString("db.Database"),
+		viper.GetString("db.Charset"))
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true, //表名单数
+		},
+	})
+	//db, err := gorm.Open(mysql.New(mysql.Config{
+	//	DSN:                       "root:123456@tcp(127.0.0.1:3306)/crud-list?charset=utf8mb4&parseTime=True&loc=Local", // DSN data source name
+	//	DefaultStringSize:         256,                                                                                  // string 类型字段的默认长度
+	//	DisableDatetimePrecision:  true,                                                                                 // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
+	//	DontSupportRenameIndex:    true,                                                                                 // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
+	//	DontSupportRenameColumn:   true,                                                                                 // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
+	//	SkipInitializeWithVersion: false,                                                                                // 根据当前 MySQL 版本自动配置
+	//}), &gorm.Config{})
+
+	sqlDB, err := db.DB()
+
+	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+	sqlDB.SetMaxIdleConns(10)
+	// SetMaxOpenConns sets the maximum number of open connections to the database.
+	sqlDB.SetMaxOpenConns(100)
+	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	return db, err
+}
+
+func CreateDB(db *gorm.DB, model *any) error {
+	err := db.AutoMigrate(model)
+	return err
+}
+
+func init() {
+
+}
+
+func main() {
+	db, err := ConnectToDatabase()
+	if err != nil {
+		fmt.Println("Error connecting to database:", err)
+		return
+	}
+
+	// 数据库迁移
+	err = db.AutoMigrate(&CrudList{})
+}
