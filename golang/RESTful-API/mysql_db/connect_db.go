5de937918af1da7ff3a4a70b8ab20ebcddb4773a
luke <luke-github@outlook.com>
2025-03-15 21:01:49 +0800

    update mysql-connect remark

--- golang/RESTful-API/mysql_db/connect_db.go
+++ golang/RESTful-API/mysql_db/connect_db.go
@@ -1,70 +1,79 @@
 package mysql_db
 
-//package main
+// package main // 这个注释掉的行表示该文件原本可以作为独立的 Go 可执行程序，但目前作为包导入使用
 
 import (
 	"fmt"                    // 标准库，提供格式化 I/O 功能，例如打印日志和调试输出
-	"github.com/spf13/viper" // Viper 库，用于读取和解析配置文件（YAML、JSON、TOML 等）
+	"github.com/spf13/viper" // Viper 库，用于读取和解析配置文件（支持 YAML、JSON、TOML 等）
 	"gorm.io/driver/mysql"   // GORM 的 MySQL 连接驱动，用于连接 MySQL 数据库
 	"gorm.io/gorm"           // GORM（Go 的 ORM 框架），用于数据库操作
 	"gorm.io/gorm/schema"    // GORM 的 Schema 相关功能，比如自定义表名规则
 	"time"                   // 标准库，提供时间和日期处理
 )
 
+// 连接数据库函数，返回 *gorm.DB 连接实例和可能的错误
 func ConnectToDatabase() (*gorm.DB, error) {
-	//链接数据库
+	// 连接数据库
 	// 参考 https://github.com/go-sql-driver/mysql#dsn-data-source-name 获取详情
-	//dsn := "root:123456@tcp(127.0.0.1:3306)/crud-list?charset=utf8mb4&parseTime=True&loc=Local"
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=True&loc=Local",
-		viper.GetString("db.UserName"),
-		viper.GetString("db.Password"),
-		viper.GetString("db.Host"),
-		viper.GetString("db.Port"),
-		viper.GetString("db.Database"),
-		viper.GetString("db.Charset"))
 
+	// 构造 MySQL DSN（数据源名称），格式为：
+	// "用户名:密码@tcp(主机:端口)/数据库名?charset=编码&parseTime=True&loc=Local"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=True&loc=Local",
+		viper.GetString("db.UserName"), // 从 Viper 配置文件获取数据库用户名
+		viper.GetString("db.Password"), // 获取数据库密码
+		viper.GetString("db.Host"),     // 获取数据库主机地址
+		viper.GetString("db.Port"),     // 获取数据库端口
+		viper.GetString("db.Database"), // 获取数据库名称
+		viper.GetString("db.Charset"))  // 获取数据库字符集
+
+	// 使用 GORM 连接 MySQL 数据库
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		NamingStrategy: schema.NamingStrategy{
-			SingularTable: true, //表名单数
+			SingularTable: true, // 设置表名为单数形式，例如 `user` 而不是 `users`
 		},
 	})
-	//db, err := gorm.Open(mysql.New(mysql.Config{
-	//	DSN:                       "root:123456@tcp(127.0.0.1:3306)/crud-list?charset=utf8mb4&parseTime=True&loc=Local", // DSN data source name
-	//	DefaultStringSize:         256,                                                                                  // string 类型字段的默认长度
-	//	DisableDatetimePrecision:  true,                                                                                 // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
-	//	DontSupportRenameIndex:    true,                                                                                 // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
-	//	DontSupportRenameColumn:   true,                                                                                 // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
-	//	SkipInitializeWithVersion: false,                                                                                // 根据当前 MySQL 版本自动配置
-	//}), &gorm.Config{})
+	if err != nil {
+		return nil, err // 连接失败，返回错误
+	}
 
+	// 获取底层 SQL 数据库对象
 	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err // 获取失败，返回错误
+	}
 
-	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
-	sqlDB.SetMaxIdleConns(10)
-	// SetMaxOpenConns sets the maximum number of open connections to the database.
-	sqlDB.SetMaxOpenConns(100)
-	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
-	sqlDB.SetConnMaxLifetime(time.Hour)
+	// 设置数据库连接池参数
+	sqlDB.SetMaxIdleConns(10)           // 设置空闲连接池的最大连接数
+	sqlDB.SetMaxOpenConns(100)          // 设置最大打开的连接数
+	sqlDB.SetConnMaxLifetime(time.Hour) // 设置连接的最大生命周期，避免连接长期占用资源
 
-	return db, err
+	return db, nil // 返回数据库连接实例
 }
 
+// 数据库自动迁移函数
 func CreateDB(db *gorm.DB, model *any) error {
+	// 使用 GORM 的 AutoMigrate 进行数据库迁移（自动创建表结构）
 	err := db.AutoMigrate(model)
 	return err
 }
 
+// init 函数：Go 语言的特殊函数，在包初始化时自动执行
 func init() {
-
+	// 这里可以放置初始化逻辑，例如加载配置文件
 }
 
+// 主函数
 func main() {
+	// 连接数据库
 	db, err := ConnectToDatabase()
 	if err != nil {
 		fmt.Println("Error connecting to database:", err)
 		return
 	}
 
-	// 数据库迁移
+	// 数据库迁移（创建 `CrudList` 表）
 	err = db.AutoMigrate(&CrudList{})
+	if err != nil {
+		fmt.Println("Error migrating database:", err)
+	}
 }
